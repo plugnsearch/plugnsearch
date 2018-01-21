562a2e0317125106e4194e7b8672b3432fffb432
Calamari <g.tavonius@gmail.com>
2018-01-21 23:46:33 +0100

    Make utils dataExtractor bit more simpler

--- src/utils/dataExtractor.js
+++ src/utils/dataExtractor.js
@@ -1,18 +1,16 @@
 import cheerio from 'cheerio'
 
 export default (html, basePath = null) => {
-  return new Promise(function (resolve) {
-    const $ = cheerio.load(html)
-    resolve({
-      title: $('title').text(),
-      description: $('meta[name=description]').attr('content'),
-      robots: $('meta[name=robots]').attr('content'),
-      canonical: $('link[rel=canonical]').attr('href'),
-      jsonLd: $('script[type="application/ld+json"]')
-        .map((i, el) => (
-          JSON.parse($(el).html())
-        ))
-        .toArray()
-    })
+  const $ = cheerio.load(html)
+  return Promise.resolve({
+    title: $('title').text(),
+    description: $('meta[name=description]').attr('content'),
+    robots: $('meta[name=robots]').attr('content'),
+    canonical: $('link[rel=canonical]').attr('href'),
+    jsonLd: $('script[type="application/ld+json"]')
+      .map((i, el) => (
+        JSON.parse($(el).html())
+      ))
+      .toArray()
   })
 }
