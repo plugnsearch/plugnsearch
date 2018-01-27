09ca79678aae6498ab4e44d46e482d4e65c20d88
Calamari <g.tavonius@gmail.com>
2018-01-27 15:25:33 +0100

    Add index.js for exporting Crawler to outside

---
+++ index.js
@@ -0,0 +1,15 @@
+export default from './src/Crawler'
+
+export Blacklist from './src/middlewares/Blacklist'
+export HttpLinkExpander from './src/middlewares/HttpLinkExpander'
+export OnlyDownloadSpecificTypes from './src/middlewares/OnlyDownloadSpecificTypes'
+export RobotsTxtAdvisor from './src/middlewares/RobotsTxtAdvisor'
+export RotateUserAgent from './src/middlewares/RotateUserAgent'
+export ThrottleRequests from './src/middlewares/ThrottleRequests'
+
+export JSONReporter from './src/reporters/JSONReporter'
+export JSONStreamReporter from './src/reporters/JSONStreamReporter'
+
+export checkContentType from './src/utils/checkContentType'
+export dataExtractor from './src/utils/dataExtractor'
+export linkExtractor from './src/utils/linkExtractor'
