d5f0db288b1540d6b506a970af5337d332885424
Calamari <g.tavonius@gmail.com>
2018-03-23 10:45:30 +0100

    Add limit based file splitting to JSONStreamReporter
    This should ensure, that we always have manageable report files. Plus we ensure that the filename always ends on `.json`.

--- src/reporters/JSONStreamReporter.js
+++ src/reporters/JSONStreamReporter.js
@@ -1,29 +1,55 @@
 import fs from 'fs'
 
+const JSON_REGEX = /\.json$/
+
 export default class JSONStreamReporter {
-  constructor ({ filename }) {
-    this.filename = filename
+  constructor ({ filename, fileLimitSize = null }) {
+    this.filename = JSON_REGEX.test(filename) ? filename : `${filename}.json`
+    this.fileLimitSize = fileLimitSize
+    this.chunkNumber = 1
+    this.files = []
+    this.openNewFile()
+  }
+
+  openNewFile () {
+    const filename = this.chunkNumber === 1 ? this.filename : this.filename.replace(JSON_REGEX, `-${this.chunkNumber}.json`)
     this.stream = fs.createWriteStream(filename)
     this.stream.write('[')
+    this.files.push(filename)
     this.alreadyWritten = false
+    this.writtenData = 1
+    ++this.chunkNumber
   }
 
   report (url, type, content) {
     if (this.alreadyWritten) {
       // make it valid json
       this.stream.write(', ')
+      this.writtenData += 2
     }
-    this.stream.write(JSON.stringify({
+    const dataToWrite = JSON.stringify({
       url: url.toString(),
       [type]: content
-    }, null, 2))
+    }, null, 2)
+    this.writtenData += dataToWrite.length
+    this.stream.write(dataToWrite)
     this.alreadyWritten = true
+    if (this.fileLimitSize && this.writtenData >= this.fileLimitSize) {
+      this.lastStream = this.stream.end(']')
+      this.openNewFile()
+    }
   }
 
   closeStream () {
     return new Promise(resolve => {
       this.stream
-        .on('finish', resolve)
+        .on('finish', () => {
+          if (this.lastStream) {
+            this.lastStream.on('finish', resolve)
+          } else {
+            resolve()
+          }
+        })
         .end(']')
     })
   }
--- test/reporters/JSONStreamReporter.spec.js
+++ test/reporters/JSONStreamReporter.spec.js
@@ -90,4 +90,89 @@ describe('Reporter', () => {
         done()
       })
   })
+
+  describe('when having set a fileSizeLimit', () => {
+    beforeEach(() => {
+      filename = path.join(tmpPath, 'limited-stream-report.json')
+      reporter = new Reporter({
+        filename,
+        fileLimitSize: 200 // bytes
+      })
+
+      reporter.report(new URL('http://something.com'), 'one', 'foo bar baz')
+      reporter.report(new URL('http://something.com'), 'two', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae dolores distinctio luftballon.')
+      reporter.report(new URL('http://something.com'), 'three', 'Just something more')
+    })
+    afterEach(() => {
+      try {
+        fs.unlinkSync(path.join(tmpPath, 'limited-stream-report.json'))
+        fs.unlinkSync(path.join(tmpPath, 'limited-stream-report-2.json'))
+      } catch (e) {}
+    })
+
+    it('splits to valid JSON files after reaching the limit', done => {
+      expect.assertions(5)
+      reporter.closeStream()
+        .then(() => {
+          const writtenData1 = fs.readFileSync(filename, 'UTF-8')
+          const writtenData2 = fs.readFileSync(filename.replace('.json', '-2.json'), 'UTF-8')
+          const jsonData1 = JSON.parse(writtenData1)
+          const jsonData2 = JSON.parse(writtenData2)
+
+          expect(writtenData1.length).toEqual(204)
+          expect(writtenData2.length).toEqual(71)
+
+          expect(jsonData1[0]).toEqual({
+            url: 'http://something.com',
+            one: 'foo bar baz'
+          })
+          expect(jsonData1[1]).toEqual({
+            url: 'http://something.com',
+            two: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae dolores distinctio luftballon.'
+          })
+          expect(jsonData2[0]).toEqual({
+            url: 'http://something.com',
+            three: 'Just something more'
+          })
+          done()
+        })
+    })
+
+    it('returns all the filenames it writes', (done) => {
+      expect.assertions(1)
+      reporter.closeStream()
+        .then(() => {
+          expect(reporter.files).toEqual([
+            path.join(tmpPath, 'limited-stream-report.json'),
+            path.join(tmpPath, 'limited-stream-report-2.json')
+          ])
+          done()
+        })
+    })
+  })
+
+  describe('when the given filename does not end on .json', () => {
+    beforeEach(() => {
+      filename = path.join(tmpPath, 'test-stream-report')
+      reporter = new Reporter({
+        filename,
+        fileLimitSize: 200 // bytes
+      })
+    })
+    afterEach(() => {
+      try {
+        fs.unlinkSync(filename + '.json')
+      } catch (e) {}
+    })
+
+    it('still creates a file with json ending', (done) => {
+      expect.assertions(1)
+      reporter.closeStream()
+        .then(() => {
+          const writtenData = JSON.parse(fs.readFileSync(filename + '.json', 'UTF-8'))
+          expect(writtenData.length).toEqual(0)
+          done()
+        })
+    })
+  })
 })
